b610aabf83ee0508e79f83f5fdcf176be114c894
hainesma <hainesma@gmail.com>
2021-06-30 17:13:52 -0400

    Add project files.

---
+++ HouseStore/Controllers/StorefrontController.cs
@@ -0,0 +1,47 @@
+﻿using HouseStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseStore.Controllers
+{
+    public class StorefrontController : Controller
+    {
+
+        HousesContext db = new HousesContext();
+        public IActionResult Index()
+        {
+            List<House> houses = db.Houses.ToList();
+            return View(houses);
+        }
+
+        public IActionResult Buy(int id)
+        {
+            House h = db.Houses.Find(id);
+            return View(h);
+        }
+
+        [HttpPost]
+        public IActionResult Buy(House h)
+        {
+            h.Quantity = 0;
+            db.Houses.Update(h);
+            return RedirectToAction("Index", "Storefront");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HouseStore/Models/StateEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HouseStore.Models
+{
+    public partial class StateEnum
+    {
+        public StateEnum()
+        {
+            Houses = new HashSet<House>();
+        }
+
+        public string State { get; set; }
+
+        public virtual ICollection<House> Houses { get; set; }
+    }
+}
