34b37e75a9e20dbf9840227618af33ac9427f408
hainesma <hainesma@gmail.com>
2021-07-01 08:57:22 -0400

    Delete function

---
+++ HouseStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using HouseStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        HousesContext db = new HousesContext();
+        
+        public IActionResult Index()
+        {
+            List<House> houses = db.Houses.ToList();
+            return View(houses);
+        }
+    }
+}
