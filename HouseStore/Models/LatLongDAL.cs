f4443e49f37e874cd54496143018e6c5382aee1f
Mark Haines <hainesma@gmail.com>
2021-08-26 20:15:39 -0400

    Added LatLongDAL

---
+++ HouseStore/Models/LatLongDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseStore.Models
+{
+    public class LatLongDAL
+    {
+        static string JSON
+    }
+}
